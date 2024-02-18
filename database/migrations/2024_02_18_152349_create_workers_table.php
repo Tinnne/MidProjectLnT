159188caa915c7ea45d9e72539612fd8be7de8c8
Tinnne <austin.kane1812@gmail.com>
2024-02-18 22:59:41 +0700

    breeze

--- database/migrations/2024_02_18_152349_create_workers_table.php
+++ database/migrations/2024_02_18_152349_create_workers_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('workers', function (Blueprint $table) {
             $table->id();
+            $table->string('nama');
+            $table->string('foto')->nullable();
+            $table->integer('umur');
+            $table->string('alamat');
+            $table->string('nomor');
             $table->timestamps();
         });
     }
